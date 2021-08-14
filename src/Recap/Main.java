2f54c3689e6b4e3380c0a2aeed9b5a2994bcb589
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-13 20:37:12 -0500

    for loops continued

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -7,10 +7,19 @@ public class Main {
     public static void main(String[] args){
 
         int[] arr = {1,5,7,3,4,5,};
+        String[] names = new String[5];
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i] == 5) {
-                System.out.println("Found a 5! at index " + i);
+        for (int i = 0; i < names.length; i++){
+            System.out.println("Input: ");
+            String input = sc.nextLine();
+            names[i] = input;
+        }
+
+        for (String n:names) {
+            System.out.println(n);
+            if (n.equals("sergio")){
+                break;
             }
         }
 

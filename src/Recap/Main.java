38604fb4bf357b73280c93f5f3534ba5404e3db4
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-13 20:38:02 -0500

    while loops

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -6,21 +6,11 @@ public class Main {
 
     public static void main(String[] args){
 
-        int[] arr = {1,5,7,3,4,5,};
-        String[] names = new String[5];
         Scanner sc = new Scanner(System.in);
+        int x = 0;
 
-        for (int i = 0; i < names.length; i++){
-            System.out.println("Input: ");
-            String input = sc.nextLine();
-            names[i] = input;
-        }
-
-        for (String n:names) {
-            System.out.println(n);
-            if (n.equals("sergio")){
-                break;
-            }
+        while (x<=10){
+            x += 1;
         }
 
     }

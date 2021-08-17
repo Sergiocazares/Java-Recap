7c68115d05af203d6360f3c74640766c07996b11
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-16 20:59:48 -0500

    hash tables and maps

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -1,25 +1,13 @@
 package Recap;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        List<Integer> t = new LinkedList<Integer>();
-        t.add(1);
-        t.add(2);
-        t.add(1);
-        t.add(2);
-        t.add(1);
-        t.add(2);
-        t.get(0);
-        t.set(1, 5);
-        t.subList(1, 3);
+        Map m = new HashMap();
 
-        System.out.println(t.subList(1, 4));
     }
 
 }

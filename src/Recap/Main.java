820c38c81e01f936407ea7ee5a15b404247be645
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-16 21:01:15 -0500

    Maps Examples

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -1,12 +1,20 @@
 package Recap;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Map m = new HashMap();
+        int[] x = {-99,5,6,3,2,1,7,8,0};
+
+        Arrays.sort(x, 1, 7);
+
+        for (int i:x)
+        {
+            System.out.println(i + ",");
+        }
 
     }
 

3d159f8252cc5630e8d40676bc04140b9efe3d61
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-16 21:03:03 -0500

    intro objects

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -7,14 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] x = {-99,5,6,3,2,1,7,8,0};
+        Scanner sc = new Scanner(System.in);
+        String h = "hello";
+        h.length();
 
-        Arrays.sort(x, 1, 7);
-
-        for (int i:x)
-        {
-            System.out.println(i + ",");
-        }
+        System.out.println(str("hi"));
 
     }
 

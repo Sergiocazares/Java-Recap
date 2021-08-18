49be6c0c8a11b5ecebd11b3bba5094ce8ff8ba19
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-17 21:30:02 -0500

    age and name dog

--- src/Recap/Dog.java
+++ src/Recap/Dog.java
@@ -1,4 +1,13 @@
 package Recap;
 
 public class Dog {
+
+    private String name;
+    private int age;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
 }

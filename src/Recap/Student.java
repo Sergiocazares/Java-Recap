4d8310049eba1b4869a1bd402e5b2004b19d4c20
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-08-26 20:47:09 -0500

    boolean student

--- src/Recap/Student.java
+++ src/Recap/Student.java
@@ -4,8 +4,17 @@ public class Student {
 
     private String name;
 
-    public Student(String name){
+    public Student(String name) {
         this.name = name;
+    }
+
+    public boolean equals(Student other){
+        if (this.name == other.name){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
